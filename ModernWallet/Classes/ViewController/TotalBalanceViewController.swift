ff8a81e01291632f20211e83dfcce4e7f8655783
Lyubomir Marinov <lyubomir.tsekov@primeholding.com>
2018-07-26 15:36:38 +0300

    LMW-523 - keep the ```// TODO: ``` comments

--- ModernWallet/Classes/ViewController/TotalBalanceViewController.swift
+++ ModernWallet/Classes/ViewController/TotalBalanceViewController.swift
@@ -32,7 +32,7 @@ class TotalBalanceViewController: UIViewController {
             .drive(currencyCode.rx.text)
             .disposed(by: disposeBag)
 
-// NOTE: Move loading indicator in parent controller (this one where resides Total Balance view controller)
+// TODO: Move loading indicator in parent controller (this one where resides Total Balance view controller)
 //        viewModel.loading.isLoading
 //            .bind(to: rx.loading)
 //            .disposed(by: disposeBag)
