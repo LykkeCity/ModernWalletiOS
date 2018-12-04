61cf58a34c58d94f93528d4eaf8b59170e9368b4
Georgi Stanev <georgi.stanev@primeholding.com>
2018-12-04 16:15:23 +0200

    Dummy commit

--- ModernWallet/Classes/ViewController/Portfolio/PortfolioViewController.swift
+++ ModernWallet/Classes/ViewController/Portfolio/PortfolioViewController.swift
@@ -104,7 +104,7 @@ class PortfolioViewController: UIViewController {
         
         bindViewModels()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
